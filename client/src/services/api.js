40bfe6356398f7866389de6b0de02b8b05c18565
HassanJama614 <soice3ity@gmail.com>
2025-07-02 14:10:33 +0300

    test run api.js new url piping

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api', // Fallback if .env not loaded
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api',
+    baseURL: 'https://boxingly-api.onrender.com/api', // Default base URL
 });
 
 // Interceptor to add token to requests if available
